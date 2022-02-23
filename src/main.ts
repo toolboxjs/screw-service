d3f71e97d2761620a2748840c580ead22c46438a
zekun.jin <zekun.jin@connext.com.cn>
2022-02-23 14:42:41 +0800

    feat: add global transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { TransformInterceptor } from './core/interceptors/transform.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
