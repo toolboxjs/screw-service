d89364335fc881f8e4f481333e8628d6af2dde6b
zekun.jin <zekun.jin@connext.com.cn>
2022-02-24 15:07:09 +0800

    feat: add consul module for root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConsulModule } from 'nestjs-consul';
 import { Connection } from 'typeorm';
 import { InterceptorModule } from './modules/interceptor/interceptor.module';
 import { LocalStorageModule } from './modules/local-storage/local-storage.module';
 
+interface ConsulConfig {
+  keys: { key: string }[];
+  connection: {
+    portocol: string;
+    port: number;
+    host: string;
+    token?: string;
+  };
+}
+
 @Module({
-  imports: [TypeOrmModule.forRoot(), InterceptorModule, LocalStorageModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ConsulModule.forRoot<ConsulConfig>({
+      keys: [],
+      connection: {
+        protocol: 'http',
+        port: 8500,
+        host: '192.168.0.1',
+        token: 'mutoken'
+      }
+    }),
+    InterceptorModule,
+    LocalStorageModule
+  ],
   providers: []
 })
 export class AppModule {
