deafad139fe5cd8d081b10a4a3dfc729f1f1e2be
zekun.jin <zekun.jin@connext.com.cn>
2022-02-25 14:43:39 +0800

    doc: add interceptor service js doc

--- packages/core/src/interceptor/interceptor.service.ts
+++ packages/core/src/interceptor/interceptor.service.ts
@@ -14,15 +14,30 @@ export class InterceptorService {
     private readonly interceptorRepository: Repository<InterceptorEntity>
   ) {}
 
+  /**
+   * 全量查询rest api拦截器列表
+   * @param {Record<string, any>} query - 过滤信息
+   * @returns {InterceptorsRO} 拦截器列表
+   */
   async findAll(query): Promise<InterceptorsRO> {
     const interceptors = await this.interceptorRepository.find();
     return new PaginationArray(interceptors);
   }
 
+  /**
+   * id查询rest api拦截器
+   * @param {number} id - 拦截器id
+   * @returns {InterceptorRO} 拦截器
+   */
   async findOne(id: number): Promise<InterceptorRO> {
     return await this.interceptorRepository.findOne({ id });
   }
 
+  /**
+   * 新建拦截器
+   * @param {CreateInterceptorDto} params - 拦截器信息
+   * @returns {void}
+   */
   async create(params: CreateInterceptorDto): Promise<void> {
     const interceptor = new InterceptorEntity();
     interceptor.request = params.request;
@@ -32,13 +47,17 @@ export class InterceptorService {
     return;
   }
 
+  /**
+   * 编辑拦截器
+   * @param {CreateInterceptorDto} params - 拦截器信息
+   * @returns {void}
+   */
   async update(id: number, params: UpdateInterceptorDto): Promise<void> {
     const toUpdate = await this.interceptorRepository.findOne({ id });
 
     if (!toUpdate) throw new BadRequestException();
 
     const updated = Object.assign(toUpdate, params);
-
     await this.interceptorRepository.save(updated);
     return;
   }
