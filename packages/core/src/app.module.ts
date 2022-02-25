6d77ae1f0d0ed9735dd148142d3c3210c03468fd
zekun.jin <zekun.jin@connext.com.cn>
2022-02-25 14:10:05 +0800

    feat: remove unused vars

--- packages/core/src/app.module.ts
+++ packages/core/src/app.module.ts
@@ -1,28 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConsulModule } from 'nestjs-consul';
-import { IConsulConnection } from 'nestjs-consul/dist/interfaces/consul-connection.interface';
 import { Connection } from 'typeorm';
 import { InterceptorModule } from './interceptor/interceptor.module';
 import { LocalStorageModule } from './local-storage/local-storage.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    ConsulModule.forRoot<IConsulConnection>({
-      keys: [],
-      connection: {
-        protocol: 'http',
-        port: 8500,
-        host: '10.128.0.214',
-        token: 'mutoken'
-      }
-    }),
-    InterceptorModule,
-    LocalStorageModule
-  ],
+  imports: [TypeOrmModule.forRoot(), InterceptorModule, LocalStorageModule],
   providers: []
 })
-export class AppModule {
-  constructor(private readonly connection: Connection) {}
-}
+export class AppModule {}
